359540e8f8f56ad82ee4b03e9c5296680018b195
felipe <fjuarezmurillo@synergyj.com>
2013-09-10 15:36:26 -0500

    #2 agregando creacion de pago al servicio

--- grails-app/services/com/payable/PagoService.groovy
+++ grails-app/services/com/payable/PagoService.groovy
@@ -2,8 +2,20 @@ package com.payable
 
 class PagoService {
 
-  def serviceMethod() {
+  def esquemaDePagoService
 
+  def crearPago(Date fechaDeVencimiento, Long esquemaDePagoId){
+    def esquemaDePago = EsquemaDePago.get(esquemaDePagoId)
+    def descuentoAplicable = esquemaDePagoService.obtenerCantidadDeDescuentoAplicable(esquemaDePagoId)
+    def pago = new Pago(
+      fechaDeVencimiento:fechaDeVencimiento,
+      cantidadDePago:esquemaDePago.cantidadDePago,
+      conceptoDePago:esquemaDePago.concepto.descripcion,
+      descuentoAplicable:descuentoAplicable,
+      recargo:esquemaDePago.recargo)
+    pago.descuentos = esquemaDePago.descuentos
+    pago.save()
+    pago
   }
 
 }
