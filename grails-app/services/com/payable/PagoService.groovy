6d3b5147bc29d6efe25d69141cc26217c150b390
felipe <fjuarezmurillo@synergyj.com>
2013-09-11 16:47:12 -0500

    #coding #5 externalizando y refactorizando código

--- grails-app/services/com/payable/PagoService.groovy
+++ grails-app/services/com/payable/PagoService.groovy
@@ -18,4 +18,56 @@ class PagoService {
     pago
   }
 
+  def obtenerPagoParaValidarComprobante(Long pagoId) {
+    Pago.findById(pagoId,[fetch:['comprobanteDePago':'join']])
+  }
+
+  def estadoDeCuentaUsuario(def usuario) {
+    [
+      pagosVencidos:obtenerPagosVencidos(usuario),
+      pagosEnTiempo:obtenerPagosDeUsuarioEnTiempoConDescuento(usuario,"EnTiempoConDescuento"),
+      pagosPorRealizar:obtenerPagosDeUsuarioEnTiempoSinDescuento(usuario,"EnTiempoSinDescuento"),
+      pagoMensual:obtenerPagosConciliadosFavorablemente(usuario)
+    ]
+  }
+
+  private def obtenerPagosVencidos(def usuario) {
+    def pagos = usuario.pagos
+    def pagosVencidos = pagos.findAll { pago ->
+      pago.fechaDeVencimiento <= new Date() && pago.estatusDePago == EstatusDePago.VENCIDO
+    }
+    pagosVencidos
+  }
+
+  private def obtenerPagosDeUsuarioEnTiempoConDescuento(def usuario) {
+    def pagos = usuario.pagos
+    def pagosEnTiempoConDescuento = pagos.findAll { pago ->
+      pago.fechaDeVencimiento >= new Date() && pago.estatusDePago == EstatusDePago.CREADO && pagos.descuentos
+    }
+    pagosEnTiempoConDescuento
+  }
+
+  private def obtenerPagosDeUsuarioEnTiempoSinDescuento(def usuario) {
+    def pagos = usuario.pagos
+    def pagosEnTiempoSinDescuento = pagos.findAll { pago ->
+      pago.fechaDeVencimiento >= new Date() && pago.estatusDePago == EstatusDePago.CREADO && !pagos.descuentos
+    }
+    pagosEnTiempoSinDescuento
+  }
+
+  private def obtenerPagosConciliadosFavorablemente(def usuario) {
+    def pagos = usuario.pagos
+    (minimum, maximum) = getFirstAndLastDayOfMonth()
+    def pagosConciliadosFavorablemente = pagos.findAll { pago ->
+      pago.lastUpdated >= minimum && pago.lastUpdated <= maximum && pago.estatusDePago == EstatusDePago.PAGADO
+
+    }
+    pagosConciliadosFavorablemente
+  }
+
+  private def getFirstAndLastDayOfMonth() {
+    Calendar calendar = Calendar.getInstance()
+    [calendar.getActualMinimum(Calendar.DATE), calendar.getActualMaximum(Calendar.DATE)]
+  }
+
 }
