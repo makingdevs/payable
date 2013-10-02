d1268f3c8c3224691fce5be25ee3cb53890cc5dc
neodevelop <neodevelop@gmail.com>
2013-10-01 20:13:24 -0500

    coding #7 intento de prueba para obtener pagos con descuentos vencidod

--- test/unit/com/payable/DescuentoServiceSpec.groovy
+++ test/unit/com/payable/DescuentoServiceSpec.groovy
@@ -4,7 +4,7 @@ import grails.test.mixin.*
 import spock.lang.Specification
 
 @TestFor(DescuentoService)
-@Mock([Organizacion, Descuento])
+@Mock([Organizacion, Descuento,Pago])
 class DescuentoServiceSpec extends Specification {
 
     def "Obtener todos los descuentos ligados a una organizacion"() {
@@ -24,5 +24,28 @@ class DescuentoServiceSpec extends Specification {
         def descuentoInstitucion = service.buscarDescuentosDeUnaOrganizacion(organizacion, query)
       then: "La cantidad de descuentos debe ser igual a 1"
         assert descuentoInstitucion.size() == 1
+  }
+
+  def "Obtener pagos con descuento expirado"(){
+    given:
+      def fechaDePrueba = new Date() - 7
+      def descuentoVencido = new Descuento(diasPreviosParaCancelarDescuento:7).save(validate:false)
+      def p1 = new Pago(fechaDeVencimiento:fechaDePrueba).save(validate:false)
+      def p2 = new Pago(fechaDeVencimiento:fechaDePrueba).save(validate:false)
+      def p3 = new Pago(fechaDeVencimiento:new Date()).save(validate:false)
+      p1.addToDescuentos(descuentoVencido).save(validate:false)
+      p2.addToDescuentos(descuentoVencido).save(validate:false)
+    when:
+      def pagos = service.buscarPagosConDescuentosVencidos()
+    then:
+      pagos*.id == [1,2]
+      pagos.every { pago -> pago.estatusDePago == EstatusDePago.CREADO }
+      pagos.every { pago -> 
+        pago.fechaDeVencimiento.date == fechaDePrueba.date &&
+        pago.fechaDeVencimiento.month == fechaDePrueba.month &&
+        pago.fechaDeVencimiento.year == fechaDePrueba.year 
+      }
+      Descuento.list().size() == 1
+      Pago.list().size == 3
   }
 }
