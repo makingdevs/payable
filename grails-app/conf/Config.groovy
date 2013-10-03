dc341bb6a19d193964ab6707a4bc03daaa8c9de7
neodevelop <neodevelop@gmail.com>
2013-10-02 21:20:32 -0500

    Agregando configuracion de cobertura

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,4 +1,7 @@
 // configuration for plugin testing - will not be included in the plugin zip
+coverage {
+  xml = true
+}
 
 log4j = {
     // Example of changing the log pattern for the default console
