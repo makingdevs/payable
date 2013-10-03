c8552de45b4b19f357743c62c9abeecc534c7d4b
neodevelop <neodevelop@gmail.com>
2013-10-03 10:06:38 -0500

    coding #9 quitando la generación de descuentos en el servicio de pago

--- grails-app/services/com/payable/PagoService.groovy
+++ grails-app/services/com/payable/PagoService.groovy
@@ -8,18 +8,11 @@ class PagoService {
 
   def crearPago(Date fechaDeVencimiento, Long esquemaDePagoId){
     def esquemaDePago = EsquemaDePago.get(esquemaDePagoId)
-    def descuentoAplicable = esquemaDePagoService.obtenerCantidadDeDescuentoAplicable(esquemaDePagoId)
     def pago = new Pago(
       fechaDeVencimiento:fechaDeVencimiento,
       cantidadDePago:esquemaDePago.cantidadDePago,
       conceptoDePago:esquemaDePago.concepto.descripcion,
-      descuentoAplicable:descuentoAplicable,
       recargo:esquemaDePago.recargo)
-
-    esquemaDePago.descuentos.each { descuento ->
-      pago.addToDescuentos( descuento )
-    }
-
     pago.save()
     pago
   }
