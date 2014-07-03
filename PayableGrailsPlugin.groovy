4a73cc9a3c5a0b676059735c69ac6dc3419289f9
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-07-03 10:44:13 -0500

    coding #23 upgrade de la version, corrigiendo status a los pagos con fecha de vencimiento igual a la actual

--- PayableGrailsPlugin.groovy
+++ PayableGrailsPlugin.groovy
@@ -1,7 +1,7 @@
 class PayableGrailsPlugin {
     // the version or versions of Grails the plugin is designed for
     def groupId = "com.payable"
-    def version = "0.2.19"
+    def version = "0.2.20"
     def grailsVersion = "2.2 > *"
     def pluginExcludes = [
         "grails-app/views/error.gsp"
