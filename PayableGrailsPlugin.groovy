2d8314e1e945da45a5a2b86d3f81bfc582acffc8
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-07-15 14:34:28 -0500

    coding #29 upgrade de la version

--- PayableGrailsPlugin.groovy
+++ PayableGrailsPlugin.groovy
@@ -1,7 +1,7 @@
 class PayableGrailsPlugin {
     // the version or versions of Grails the plugin is designed for
     def groupId = "com.payable"
-    def version = "0.2.23"
+    def version = "0.2.24"
     def grailsVersion = "2.2 > *"
     def pluginExcludes = [
         "grails-app/views/error.gsp"
