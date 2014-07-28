5e2bd494057aacc7dcf490ab9f9214f42d2126c4
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-07-28 16:00:19 -0500

    coding #30 agregando algunos atributos

--- grails-app/domain/com/payable/Payment.groovy
+++ grails-app/domain/com/payable/Payment.groovy
@@ -1,7 +1,29 @@
 package com.payable
 
 class Payment {
+ 
+  Date paymentDate 
+  Date dueDate
+
+  BigDecimal paymentAmount
+  BigDecimal accumulatedSurcharges = 0
+  BigDecimal applicableDiscount = 0
+  
+  String reference
+  String paymentConcept
+  String transactionId = UUID.randomUUID().toString().replaceAll('-', '').substring(0,20)
+
+  Date dateCreated
+  Date lastUpdated  
 
   static constraints = {
+    paymentConcept size:1..100,blank:false
+    paymentAmount min:1.0
+    accumulatedSurcharges()
+    applicableDiscount() 
+    paymentDate nullable:true
+    transactionId size:20..20
+    reference nullable:true
   }
+
 }
