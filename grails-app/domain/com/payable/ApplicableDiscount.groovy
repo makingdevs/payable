a713ca98cbd569a20f427a002f5b13aac9a49124
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-07-29 11:44:15 -0500

    coding #31 agregando propiedad de estatus a Descuento aplicable

--- grails-app/domain/com/payable/ApplicableDiscount.groovy
+++ grails-app/domain/com/payable/ApplicableDiscount.groovy
@@ -5,6 +5,8 @@ class ApplicableDiscount {
   Date expirationDate 
   Discount discount
 
+  ApplicableDiscountStatus applicableDiscountStatus = ApplicableDiscountStatus.VALID
+
   Date dateCreated
   Date lastUpdated
 
