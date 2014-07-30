91e95200f005ec264d8acbd856760b7bbfc98baf
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-07-30 13:13:59 -0500

    coding #33 agregando prueba para el servicio de generación de pagos

--- test/unit/com/payable/GenerationOfPaymentServiceSpec.groovy
+++ test/unit/com/payable/GenerationOfPaymentServiceSpec.groovy
@@ -3,18 +3,39 @@ package com.payable
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
-/**
- * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
- */
 @TestFor(GenerationOfPaymentService)
+@Mock([PaymentWithImplements,Payment,Discount,Concept,Surcharge,Organization,PaymentLink])
 class GenerationOfPaymentServiceSpec extends Specification {
 
-	def setup() {
-	}
+  def "Generation of payment for a litter"(){
+    setup:
+      
+      PaymentGroupCommand paymentGroupCommand = new PaymentGroupCommand(
+        paymentConcept: _paymentConcept,
+        paymentAmount: _paymentAmount,
+        discountIds: [],
+        doublePayment: [],
+        daysPaymentDue: 4,
+        dueDate: _dueDate,
+        organization: new Organization().save(validate:false),
+        instances: [new PaymentWithImplements().save()]
+      )
+    and:
+      def conceptServiceMock = mockFor(ConceptService)
+      conceptServiceMock.demand.savePaymentConcept{ organization, paymentConcept ->
+        new Concept(organization:organization,paymentConcept:paymentConcept).save(validate:false)
+      }
+      service.conceptService = conceptServiceMock.createMock()
 
-	def cleanup() {
-	}
+    when:
+      def payments = service.generatePaymentForGroup(paymentGroupCommand)
 
-	void "test something"() {
-	}
+    then:
+      assert payments.size() == _size
+      assert payments.first().id > 0
+
+    where:
+     _paymentConcept | _paymentAmount | _dueDate      || _size  
+     "PaymentConcept"|  100.00        | new Date()+7  || 1
+  }
 }
