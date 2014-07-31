4e96ab478b6d5a062393af4a35cece5667d98509
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-07-31 15:57:25 -0500

    coding #35 generando descuentos aplicables para un esquema de pago dada una fecha de referencia

--- grails-app/services/com/payable/ApplicableDiscountService.groovy
+++ grails-app/services/com/payable/ApplicableDiscountService.groovy
@@ -10,4 +10,18 @@ class ApplicableDiscountService {
     applicableDiscount
   }
 
+  def generateForPaymentWithPaymentSchemeWithReferenceDate(Long paymentSchemaId,Date referenceDate){
+    def applicableDiscounts = []
+    PaymentScheme paymentScheme = PaymentScheme.get(paymentSchemaId) 
+    log.error "Reference date ${referenceDate}"
+    paymentScheme.discounts.each{ discount ->
+      def expirationDate = (referenceDate - discount.previousDaysForCancelingDiscount) 
+      if(expirationDate.clearTime() < new Date().clearTime())
+        applicableDiscounts << generateForPaymentWithExpirationDate(expirationDate,discount.id)  
+    }
+
+    applicableDiscounts
+  }
+
+
 }
