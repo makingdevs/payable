81daf33293f78a063210e515d404f4ed7cf4a88e
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-08-06 13:10:24 -0500

    coding #38 agregando método para expirar los pagos y aplicar los recargos

--- grails-app/services/com/payable/SurchargeService.groovy
+++ grails-app/services/com/payable/SurchargeService.groovy
@@ -5,4 +5,29 @@ class SurchargeService {
   def saveSurcharge(def params){
     Surcharge surcharge = Surcharge.findAllByOrganization(params.organization) ?: new Surcharge(params).save()
   }
+ 
+  def expirePaymentsAndCalculateAccumulatedSurcharge(){
+
+    def payments = Payment.withCriteria{
+      lt("dueDate",new Date())   
+      inList("paymentStatus",[PaymentStatus.CREATED,PaymentStatus.REJECTED])
+    }
+    
+    payments.each{ payment ->
+      if(payment.surcharge)
+        payment.accumulatedSurcharges = calculateAccumulatedSurcharge(payment.surcharge, payment.paymentAmount)
+      payment.paymentStatus = PaymentStatus.EXPIRED 
+      payment.save()
+    }
+
+    payments
+  }
+
+  def calculateAccumulatedSurcharge(Surcharge surcharge, def paymentAmount){
+    if(surcharge.percentage) 
+      return ((paymentAmount * surcharge.percentage) / 100)
+    
+    surcharge.amount
+  }
+
 }
