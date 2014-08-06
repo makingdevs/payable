813c82ed9486d11969fe913eb9d33b73b2f5e953
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-08-06 11:48:50 -0500

    coding #38 agregando prueba para vencer pagos y calcular recargo

--- test/unit/com/payable/SurchargeServiceSpec.groovy
+++ test/unit/com/payable/SurchargeServiceSpec.groovy
@@ -4,7 +4,7 @@ import grails.test.mixin.TestFor
 import spock.lang.Specification
 
 @TestFor(SurchargeService)
-@Mock([Organization,Surcharge])
+@Mock([Organization,Surcharge,Payment])
 class SurchargeServiceSpec extends Specification {
 
   def "Save a surcharge if it doesn`t exist in an organization"(){
@@ -19,4 +19,24 @@ class SurchargeServiceSpec extends Specification {
       surcharge.id > 0
       assert surcharge.organization == organization
   }
+
+  void "Expire the payments if the dueDate is less than today and calculate the Surcharge"(){
+    given: "A payment with a Surcharge"
+     def payment = _payment
+     payment.surcharge = _surcharge
+     payment.save()    
+
+    when:
+      service.expirePaymentsAndCalculateAccumulatedSurcharge()
+
+    then:
+      payment.paymentAmount == _newPaymentAmount
+
+    where:
+      _payment                                                 | _surcharge                     || _newPaymentAmount
+      new Payment(dueDate: new Date()-7,paymentAmount:2000)    | new Surcharge(amount:100)      || 1900
+      new Payment(dueDate: new Date()-7,paymentAmount:5890)    | new Surcharge(percentage:10)   || 5301
+      new Payment(dueDate: new Date(),paymentAmount:4000)      | null                           || 4000
+  }
+
 }
