3df6460d3b8978f9427998d109d59f1a86ea7caa
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-08-08 12:05:59 -0500

    coding #44 actualizando TagLib

--- grails-app/taglib/com/payable/PaymentLinkTagLib.groovy
+++ grails-app/taglib/com/payable/PaymentLinkTagLib.groovy
@@ -2,9 +2,10 @@ package com.payable
 
 class PaymentLinkTagLib {
  
-  def paymentService 
+  def paymentLinkService 
+
   def classesList = { attrs, body ->
-    out << render(template:"/paymentLink/list",model:[])
+    out << render(template:"/paymentLink/list",model:[paymentLinkService.getClassesUsingPayable()])
   }
 
 }
