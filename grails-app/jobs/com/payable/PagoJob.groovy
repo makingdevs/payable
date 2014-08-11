99eae2e1e18bc197023007c029f57ed9a62f8eb1
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-08-11 11:02:00 -0500

    coding #45 agregando método para agregar recargo a los pagos

--- grails-app/jobs/com/payable/PagoJob.groovy
+++ grails-app/jobs/com/payable/PagoJob.groovy
@@ -3,14 +3,16 @@ package com.payable
 class PagoJob {
 
   def descuentoAplicableService
+  def recargoService
 
   static triggers = {
     cron name: 'myTrigger', cronExpression: "0 0 1 * * ?"
   }
 
   def execute() {
-    log.debug "Comenzando a recalcular pagos con descuentos a las ${new Date()}"
+    log.debug "Comenzando a recalcular pagos con descuentos a las ${new Date()}"    
     descuentoAplicableService.expirarDescuentosRecalcularPagos()
+    recargoService.exprirarPagosYCalcularRecargo()
     log.debug "Terminando de recalcular pagos con descuentos a las ${new Date()}"
   }
 }
