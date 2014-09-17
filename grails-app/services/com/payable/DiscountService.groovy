2f5d35bdda910aca8755f5c91c84c84e5d2508c4
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-09-17 13:12:25 -0500

    coding #52 modifying the generation of applicable discounts

--- grails-app/services/com/payable/DiscountService.groovy
+++ grails-app/services/com/payable/DiscountService.groovy
@@ -1,5 +1,7 @@
 package com.payable
 
+import java.text.SimpleDateFormat
+
 class DiscountService {
   
   def searchDiscountsOfAnOrganization(Organization organization,String stringToSearch){
@@ -9,4 +11,32 @@ class DiscountService {
     }
   }
 
+  def saveDiscountForPaymentSchemeOfOrganizationWithReferenceDate(organization,params){
+    Discount discount = Discount.findByOrganizationAndDiscountName(organization,params.discountName)
+     
+    if(!discount){
+
+      if(!params.previousDaysForCancelingDiscount){
+        def format = new SimpleDateFormat("dd/MM/yyyy")
+        
+        try{
+          def previousDays = getPreviousDaysForCancelingDiscount(format.parse(params.referenceDate),format.parse(params.expirationDate)) 
+          params.previousDaysForCancelingDiscount = previousDays 
+        }
+        catch(Exception exception){
+          params.previousDaysForCancelingDiscount = 0
+        }
+       
+      }
+      params.organization = organization
+      discount = new Discount(params).save()
+    }
+
+    discount
+  }
+
+
+  private Integer getPreviousDaysForCancelingDiscount(Date referenceDate, Date expirationDate){
+    (referenceDate ?: 0) - (expirationDate ?: 0) 
+  }
 }
