f324199addf347af514cbb47605fbafce82402f7
temocpalma <temopalmag@gmail.com>
2016-06-30 15:58:32 -0500

    #61 Cambiando comprobante a string con url del archivo

--- grails-app/domain/com/payable/Payment.groovy
+++ grails-app/domain/com/payable/Payment.groovy
@@ -1,37 +1,35 @@
 package com.payable
 
-import org.grails.s3.S3Asset
-
 class Payment {
- 
-  Date paymentDate 
+
+  Date paymentDate
   Date dueDate
 
   BigDecimal paymentAmount
   BigDecimal accumulatedSurcharges = 0
   BigDecimal accumulatedDiscount  = 0
-  
+
   String reference
   String paymentConcept
   String transactionId = UUID.randomUUID().toString().replaceAll('-', '').substring(0,20)
-  
+
   PaymentType paymentType = PaymentType.WIRE_TRANSFER
   PaymentStatus paymentStatus = PaymentStatus.CREATED
 
-  S3Asset proofOfPayment
-  
+  String proofOfPayment
+
   Surcharge surcharge
 
   static hasMany = [applicableDiscounts : ApplicableDiscount]
 
   Date dateCreated
-  Date lastUpdated  
+  Date lastUpdated
 
   static constraints = {
     paymentConcept size:1..100,blank:false
     paymentAmount min:1.0
     accumulatedSurcharges()
-    accumulatedDiscount() 
+    accumulatedDiscount()
     paymentDate nullable:true
     transactionId size:20..20
     proofOfPayment nullable:true
