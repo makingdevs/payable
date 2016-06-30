875311fb1a5568f50d54cc3331e43e78f391c85f
temocpalma <temopalmag@gmail.com>
2016-06-30 16:00:02 -0500

    #61 Ajustando métodos con service de amazon

--- grails-app/services/com/payable/ProofOfPaymentService.groovy
+++ grails-app/services/com/payable/ProofOfPaymentService.groovy
@@ -1,21 +1,11 @@
 package com.payable
 
-import org.grails.s3.S3Asset
-
 class ProofOfPaymentService {
-  
-  def s3AssetService
+  def grailsApplication
 
   def addProofOfPayment(Long paymentId, file){
-    S3Asset proofOfPayment = new S3Asset()
-    proofOfPayment.options.addAsync = 'false'
     Payment payment = Payment.get(paymentId)
-    def tmp = s3AssetService.getNewTmpLocalFile(file.contentType)
-    file.transferTo(tmp)
-    proofOfPayment.newFile(tmp)
-    proofOfPayment.mimeType = file.contentType
-    s3AssetService.put(proofOfPayment)
-    payment.proofOfPayment = proofOfPayment
+    payment.proofOfPayment = amazonService.uploadFile(file)
     payment.paymentStatus = PaymentStatus.PROCESS
     payment.save()
     payment
@@ -27,37 +17,37 @@ class ProofOfPaymentService {
     payment.paymentDate = paymentDate
     payment.reference = reference;
     if(payment.paymentType == PaymentType.CASH || payment.paymentStatus == PaymentStatus.PROCESS)
-      payment.paymentStatus = PaymentStatus.PAID 
+      payment.paymentStatus = PaymentStatus.PAID
     else
-      payment.paymentStatus = PaymentStatus.PROCESS 
+      payment.paymentStatus = PaymentStatus.PROCESS
 
     def applicableDiscounts = payment.applicableDiscounts.findAll {
-      it.applicableDiscountStatus == ApplicableDiscountStatus.VALID 
+      it.applicableDiscountStatus == ApplicableDiscountStatus.VALID
     }
 
-    applicableDiscounts.each{ applicableDiscount -> 
-      applicableDiscount.applicableDiscountStatus = ApplicableDiscountStatus.APPLIED 
+    applicableDiscounts.each{ applicableDiscount ->
+      applicableDiscount.applicableDiscountStatus = ApplicableDiscountStatus.APPLIED
     }
 
     applicableDiscounts*.save()
     payment.save()
     payment
   }
-  
+
   def rejectPayment(transactionId){
     def payment = Payment.findByTransactionId(transactionId)
     if(payment.proofOfPayment)
-      s3AssetService.delete(payment.proofOfPayment)
+      amazonService.deleteFile(payment.proofOfPayment.tokenize("/").last())
 
     payment.proofOfPayment = null
     payment.paymentStatus = PaymentStatus.REJECTED
     payment.save()
     payment
-  } 
-  
+  }
+
   def bytesOfProofOfPayment(paymentId){
     def payment = Payment.get(paymentId)
-    def url = new URL(payment.proofOfPayment.url())
+    def url = new URL(payment.proofOfPayment)
     url.getBytes()
   }
 
